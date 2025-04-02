5fcbec12ac9bd6b46ed14b0a7e1d043d3833dd5b
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-04-02 19:41:22 +0300

    Update tap on cells

--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -71,7 +71,14 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
         let title = titles[indexPath.row]
         guard let titleName = title.original_title ?? title.original_name else { return }
         
-        APICaller.shared.getMovie(with: <#T##String#>, completion: <#T##(Result<VideoElement, any Error>) -> Void#>)
+        APICaller.shared.getMovie(with: titleName + "trailer") { result in
+            switch result {
+            case .success(let videoElement):
+                print(videoElement.id)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
 }
