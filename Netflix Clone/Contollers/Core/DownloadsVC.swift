232649307b74412c7646e001241d5886c8e78961
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-04-03 22:01:58 +0300

    Integrate CoreData

--- Netflix Clone/Contollers/Core/DownloadsVC.swift
+++ Netflix Clone/Contollers/Core/DownloadsVC.swift
@@ -8,12 +8,84 @@
 import UIKit
 
 class DownloadsVC: UIViewController {
+    
+    private var titles: [TitleItem] = [TitleItem]()
 
+    private let downloadedTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        view.addSubview(downloadedTable)
         view.backgroundColor = .systemBackground
+        title = "Downloads"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        downloadedTable.delegate = self
+        downloadedTable.dataSource = self
+        fetchLocalStorageForDownloads()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        downloadedTable.frame = view.bounds
+    }
+    
+    private func fetchLocalStorageForDownloads() {
+        DataPersistenceManager.shared.fetchingFromDataBase { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.downloadedTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
+
+
+extension DownloadsVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: TitleVM(titleName: (title.original_title ?? title.original_name) ?? "Unknown title name", posterURL: title.poster_path ?? ""))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            DataPersistenceManager.shared.deleteTitleWith(model: titles[indexPath.row]) { [weak self] result in
+                switch result {
+                case .success():
+                    print("deleted from db")
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+                self?.titles.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        default:
+            break;
+            
+        }
+    }
 }
